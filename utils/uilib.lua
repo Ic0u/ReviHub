6bc61871db92bbfd89769797364ed100af180515
Nguyễn Nam <soniclolgaming@gmail.com>
2025-03-13 15:52:15 +0700

    Update uilib.lua

--- utils/uilib.lua
+++ utils/uilib.lua
@@ -427,7 +427,7 @@ local function createBind(option, parent)
         if input.UserInputType == Enum.UserInputType.MouseButton1 then
             binding = true
             bindinput.Text = "..."
-            tweenService:Create(round, TweenInfo.new(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageColor3 = Color3.fromRGB(255, 255, 255)}):Play()
+            tweenService:Create(round, TweenInfo.new(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {ImageColor3 = Color3.fromRGB(90, 90, 90)}):Play()
         end
         if input.UserInputType == Enum.UserInputType.MouseMovement then
             inContact = false
